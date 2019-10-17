848bd70362573e03e2858ccc8e3684d2bf5d7142
tuananh-nguyenk63hust <53038579+tuananh-nguyenk63hust@users.noreply.github.com>
2019-10-17 14:40:03 +0700

    Update SubscribeMosquitto.cpp

--- SubscribeMosquitto.cpp
+++ SubscribeMosquitto.cpp
@@ -1,11 +1,10 @@
 //Code by Tuan Anh Nguyen
 //Code is build with C++11
-
 #include<bits/stdc++.h>
 #include<mqtt/async_client.h>
 #include<jsoncpp/json/json.h>
 #include<fstream>
-#include<ctime>
+#include<ctime> 
 //#include<mysql/mysql.h>
 #include<mysql_connection.h>
 #include<cppconn/prepared_statement.h>
@@ -13,40 +12,24 @@
 #include<cppconn/statement.h>
 #include<cppconn/resultset.h>
 #include<cppconn/exception.h>
+#define version 100000000
 //using namespace std;
 const std::string ADDRESS="tcp://localhost:1883"; //address server
 const int QOS=0; // this's qos client subscribe
 const std::string ID="AirsenseDB"; // This's name of client subscribe mosquitto 
-const std::string TOPIC="SPARC"; // This's name of topic client subscribe 
+const std::string TOPIC="SPARC/#"; // This's name of topic client subscribe 
 const int num_of_reconnect=5;  // this's number when try connect
 //const std::string moth["January","February","March","April","May","June","July","August","September","October","November","December"]={'01','02'.'03','04','05','06','07','08','09','10','11','12'};
 //std::string file="/home/banhbanh/Desktop/"; // save raw data file edit time when receive data 
 //char *NameFile=new char[30];
 const std::string HOST="tcp://localhost";
-const std::string USER="sparclab1";
-const std::string PASS="sparclab1";
+const std::string USER="sparclab";
+const std::string PASS="SPARCLab1";
 sql::Driver *driver;
 sql::Connection *Conn;
 sql::Statement *sta;
 sql::PreparedStatement *preSta;
 sql::ResultSet *res;
-std::string ConvertMonthToInt(std::string s)
-{
-    std::string res;
-    if (s=="January") res="01";
-    if (s=="February") res="02";
-    if (s=="March") res="03";
-    if (s=="April") res="04";
-    if (s=="May") res="05";
-    if (s=="June") res="06";
-    if (s=="July") res="07";
-    if (s=="August") res="08";
-    if (s=="September") res="09";
-    if (s=="October") res="10";
-    if (s=="November") res="11";
-    if (s=="December") res="12";
-    return res;
-};
 bool CheckConnection()
 {
     try
@@ -75,82 +58,40 @@ class action_listener: public virtual mqtt::iaction_listener  // this listener w
     void on_success(const mqtt::token& tok) override {}
 public: action_listener(const std::string& name): namen(name){}  
 };
-
-std::string coverttime(long long& Time)
-{
-    putenv("TZ=Asia/Ho_Chi_Minh");
-    time_t ttime= Time;
-    std::string NameFile="";
-    tm * CoverTimee;
-    CoverTimee=localtime(&ttime);
-    char *NameYear=new char[5];
-   // for (int i=0;i<=32;i++) NameFile1[i]="";
-    strftime(NameYear,5,"%Y",CoverTimee);
-    //std::cout<<NameYear<<std::endl;
-    char *NameMoth=new char[10];
-    strftime(NameMoth,10,"%B",CoverTimee);
-    char *NameDay=new char[5];
-    strftime(NameDay,5,"%d",CoverTimee);
-    char *NameHours=new char[5];
-    strftime(NameHours,5,"%H",CoverTimee);
-    char *NameMinute=new char[3];
-    strftime(NameMinute,3,"%M",CoverTimee);
-    //NameFile+=NameMoth;
-    std::string STR_NameMoth=ConvertMonthToInt(NameMoth);    
-    //std::cout<<STR_NameMoth<<std::endl;
-    NameFile+=NameYear;
-    NameFile+="-"+STR_NameMoth+"-"+NameDay+" "+NameHours+":"+NameMinute+":00";
-    //NameFile=NameHours;
-    std::cout<<NameFile<<std::endl;
-    return NameFile;
-}
-bool checkretain(std::string ttime,std::string iid)
-{
-    std::string timelate="";
-    std::string IDLate="";
-    //std::cout<<"ok"<<std::endl;
-    preSta=Conn->prepareStatement("SELECT * FROM Data");
-    res=preSta->executeQuery();
-    res->afterLast();
-    //std::cout<<"ok"<<std::endl;
-    while(res->previous())
-    {
-        timelate=res->getString("Time");
-        IDLate=res->getString("NodeId");
-        break;
-    }
-    std::cout<<timelate<<std::endl;
-    std::cout<<IDLate<<std::endl;
-    if ((timelate==ttime)&&(iid==IDLate))
-     {  
-         std::cout<<"false"<<std::endl;
-         return false;
-     }
-    return true;
-}
 void DBReceive(Json::Value JsonData)
 {
-    std::string DBID=JsonData["NAME"].asString();
-    long long realtime=JsonData["DATA"]["REALTIMES"].asInt64();
-    float PM2p5=JsonData["DATA"]["PM2.5"].asFloat();
-    float PM10=JsonData["DATA"]["PM10"].asFloat();
-    float PM1=JsonData["DATA"]["PM1"].asFloat();
-    float CO=JsonData["DATA"]["CO"].asFloat();
-    float TEM=JsonData["DATA"]["TEM"].asFloat();
-    float HUM=JsonData["DATA"]["HUM"].asFloat();
-    //driver=get_driver_instance();
-    //Conn=driver->connect(HOST,USER,PASS);
+    long long NodeId;
+    long long realtime;
+    float PM2p5;
+    float PM10;
+    float PM1;
+    float CO;
+    float TEM;
+    float HUM;
+    try
+    {
+        std::string DBID=JsonData["NAME"].asString();
+    //long long NodeId;
+    std::stringstream stream;
+    stream<<std::hex<<DBID;
+    stream>>NodeId;
+    NodeId+=version;
+    realtime=JsonData["DATA"]["REALTIMES"].asInt64();
+    PM2p5=JsonData["DATA"]["PM2.5"].asFloat();
+    PM10=JsonData["DATA"]["PM10"].asFloat();
+    PM1=JsonData["DATA"]["PM1"].asFloat();
+    CO=JsonData["DATA"]["CO"].asFloat();
+    TEM=JsonData["DATA"]["TEM"].asFloat();
+    HUM=JsonData["DATA"]["HUM"].asFloat();
     if (CheckConnection())
     {
         std::cout<<Conn<<std::endl;
         Conn->setSchema("Airsense");
-        std::string timeconvert=coverttime(realtime);
-        if (checkretain(timeconvert,DBID))
-        {
-            std::cout<<"Accept To DB"<<std::endl;
+       // std::string timeconvert=coverttime(realtime);
+            std::cout<<"INSERT INTO DB"<<std::endl;
             preSta=Conn->prepareStatement("INSERT INTO Data(NodeId,Time,PM2p5,PM10,PM1,TEMPERATURE,HUMIDITY) VALUES(?,?,?,?,?,?,?)");
-            preSta->setString(1,DBID);
-            preSta->setDateTime(2,timeconvert);
+            preSta->setInt64(1,NodeId);
+            preSta->setInt(2,realtime);
             preSta->setDouble(3,PM2p5);
             preSta->setDouble(4,PM10);
             preSta->setDouble(5,PM1);
@@ -160,14 +101,27 @@ void DBReceive(Json::Value JsonData)
             preSta->executeUpdate();
             preSta=Conn->prepareStatement("INSERT INTO ExtendedData(NodeId,Time,CO,CO2,SO2,NO2,O3) VALUES(?,?,?,?,?,?,?)");
             preSta->setString(1,DBID);
-            preSta->setDateTime(2,timeconvert);
+            preSta->setInt(2,realtime);
             preSta->setDouble(3,CO);
             preSta->setDouble(4,0);
             preSta->setDouble(5,0);
             preSta->setDouble(6,0);
             preSta->setDouble(7,0);
-        }
-        else std::cout<<"Don't Accept To DB"<<std::endl;
+    }
+
+    
+    }
+    catch(const Json::LogicError& e)
+    {
+        std::cerr<<"Can't conver Jsonfile"<<std::endl;
+    }
+    catch(const Json::RuntimeError& e)
+    {
+        std::cerr<<"Time Error"<<std::endl;
+    }
+    catch(const sql::SQLException& e)
+    {
+        std::cerr<<"Error Logic SQL"<<std::endl;
     }
 
 }
@@ -225,24 +179,6 @@ void DBReceive(Json::Value JsonData)
         reader.parse(Payload_byte,MessageData);
         std::cout<<MessageData.toStyledString()<<std::endl;
         DBReceive(MessageData);
-        //Time=time(nullptr);
-        //std::cout<<Time<<std::endl;
-        /*std::string LastFile=file;
-        std::string namefile=coverttime(Time);
-       // std::cout<<namefile<<std::endl;
-        file+=namefile;
-        std::cout<<file<<std::endl;
-        bool BcheckFile=checkFile(file);
-        std::ofstream fo(file,std::ios::app);
-        if (BcheckFile==false) 
-        {
-            std::cout<<"Create File!"<<std::endl;
-            fo<<"ID\tREALTIMES\tPM2.5\tPM10\tPM1\tCO\tHUM\tTEM\n";
-        } 
-
-        fo<<ID_Cli<<"\t"<<Time<<"\t"<<Pm2p5<<"\t"<<Pm10<<"\t"<<Pm1<<"\t"<<Co<<"\t"<<Hum<<"\t"<<Tem<<"\n";
-        file=LastFile;
-        //char * NameFile=new char[23];*/
         
     }
 public: callback(mqtt::async_client& cli, mqtt::connect_options& cOn): num(0),cli_(cli), Conn(cOn), listener("sublistener") {}
@@ -250,14 +186,14 @@ public: callback(mqtt::async_client& cli, mqtt::connect_options& cOn): num(0),cl
 
 int main()
 {
-    
+   
+        mqtt::async_client cli(ADDRESS,ID); // client subscribe
+        mqtt::connect_options conn;
+        callback cb(cli,conn);
+        cli.set_callback(cb);
+        cli.connect(conn,nullptr,cb);
     //Conn->setSchema("Airsense");
-    mqtt::async_client cli(ADDRESS,ID); // client subscribe
-    mqtt::connect_options conn;
-    //cli.connect(conn);
-    callback cb(cli,conn);
-    cli.set_callback(cb);
-    cli.connect(conn,nullptr,cb);
+    
     while (tolower(std::cin.get())!='q');
     
 }
